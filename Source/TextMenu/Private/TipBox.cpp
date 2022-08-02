42a2a6260b5ab13c0794737bc2f05e80e33e37aa
xmiaomiao5526 <1833448004@qq.com>
2022-08-02 20:43:10 +0800

    20220802

--- Source/TextMenu/Private/TipBox.cpp
+++ Source/TextMenu/Private/TipBox.cpp
@@ -12,7 +12,7 @@ ATipBox::ATipBox()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	Tips = FText::FromString(" ");
 	TriggerBox = CreateDefaultSubobject<UBoxComponent>("TriggerBox");
 
 }
